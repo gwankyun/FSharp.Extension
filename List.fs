8d90ebf09f30739f28db5b8f0ede2b23ea0d3663
gwankyun <kyunsiu@gmail.com>
2016-10-30 19:32:16 +0800

    增加List.tryZip

--- List.fs
+++ List.fs
@@ -14,3 +14,8 @@ let tryZipList (list : 'a list list) =
             go tail (head :: result)
         | false -> result
     go list []
+
+let tryZip (list1 : 'a list) (list2 : 'a list) =
+    [list1; list2]
+    |> tryZipList
+    |> List.map (fun x -> (x.[0], x.[1]))
