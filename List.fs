31bd41e6b3f5da476ae998acd05e6bdb28986415
gwankyun <kyunsiu@gmail.com>
2016-10-30 19:31:13 +0800

    增加List.tryZipList

--- List.fs
+++ List.fs
@@ -4,3 +4,13 @@ let tryTail (list : 'a list) =
     match list with
     | h :: t -> t
     | [] -> []
+
+let tryZipList (list : 'a list list) =
+    let rec go data result =
+        match List.exists (List.isEmpty >> not) list with
+        | true ->
+            let head = List.map List.tryHead data
+            let tail = List.map tryTail data
+            go tail (head :: result)
+        | false -> result
+    go list []
