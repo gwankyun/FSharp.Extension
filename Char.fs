051392e2f291dedd5a47677b71d4087b34ca330f
gwankyun <kyunsiu@gmail.com>
2016-12-04 12:31:13 +0800

    將Char的靜態方法都改成函數

--- Char.fs
+++ Char.fs
@@ -6,3 +6,45 @@ let isLeft (c : char) =
 
 let isRight (c : char) =
     "yuiophjkl;nm,./".Contains(string c)
+
+let isControl (c : char) =
+    Char.IsControl(c)
+
+let isDigit (c : char) =
+    Char.IsDigit(c)
+
+let isHighSurrogate (c : char) =
+    Char.IsHighSurrogate(c)
+
+let isLetter (c : char) =
+    Char.IsLetter(c)
+
+let isLetterOrDigit (c : char) =
+    Char.IsLetterOrDigit(c)
+
+let isLower (c : char) =
+    Char.IsLower(c)
+
+let isLowSurrogate (c : char) =
+    Char.IsLowSurrogate(c)
+
+let isNumber (c : char) =
+    Char.IsNumber(c)
+
+let isPunctuation (c : char) =
+    Char.IsPunctuation(c)
+
+let isSeparator (c : char) =
+    Char.IsSeparator(c)
+
+let isSurrogate (c : char) =
+    Char.IsSurrogate(c)
+
+let isSymbol (c : char) =
+    Char.IsSymbol(c)
+
+let isUpper (c : char) =
+    Char.IsUpper(c)
+
+let isWhiteSpace (c : char) =
+    Char.IsWhiteSpace(c)
